403c54e6ac50edb64463a22042bea83df2dae5a2
Null <161325276+Factorito@users.noreply.github.com>
2025-10-18 23:49:08 +0900

    Refactor newton_raphson function for error calculation

--- newton_raphson.m
+++ newton_raphson.m
@@ -1,15 +1,16 @@
 function v = newton_raphson(f,df,x0,es,maxit)
-
+%function v = newton_raphson(f,df,x0,es,maxit,dx)
 
 fprintf("iteration |  x1     rerr\n");
 for i= 1:maxit
   x1 = x0-f(x0)/df(x0);
   %x1 = x0-(dx*f(x0))/(f(x0+dx)-f(x0)) 
-  ea = err(x0,x1);
+  ea = err(x1,x0);
   fprintf("%d    %7.3f    %10.7f\n",i,x1,ea);
   x0 = x1;
   if ea<es break; end
 end
+
 
 
 

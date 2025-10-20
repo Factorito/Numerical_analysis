974afb2d772e6e23a1281ddf6220beda0922b1f6
Null <161325276+Factorito@users.noreply.github.com>
2025-10-20 18:02:24 +0900

    Update forw.m

--- forw.m
+++ forw.m
@@ -5,13 +5,3 @@ function x=forw(L,b,n)
     x(i)=b(i)-L(i,1:i-1) * x(1:i-1);
   endfor
 
-
-
-function x=backw(U,d,n)
-  x = zeros(n,1);
-  x(1)=d(1)/U(1,1);
-  for i=n-1:-1:1
-    x(i)=(d(i)-U(i,i+1:n) * x(i+1:n))/U(i,i);
-  endfor
-
-

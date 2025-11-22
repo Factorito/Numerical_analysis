4cc12a2fd754018f547671d2f4975ffbf159819c
Null <161325276+Factorito@users.noreply.github.com>
2025-11-22 11:37:59 +0900

    Add files via upload

---
+++ lagrange_poly.m
@@ -0,0 +1,16 @@
+function yint = lagrange_poly(x,f,k)
+
+   n = length(x)
+   s=0;
+   for i = 1:n
+     product = f(i);
+     for j = 1:n
+       if i ~= j
+         product = product*(k-x(j))/(x(i)-x(j));
+       endif
+     endfor
+     s = s + product;
+   endfor
+   yint = s;
+
+
